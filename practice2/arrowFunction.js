e797c2c43742a7e2195a15461e9c8283f8560b60
Saumya-Jain-B2K <saumyajain2020@gmail.com>
2025-11-11 22:17:15 +0530

    new practice

--- practice2/arrowFunction.js
+++ practice2/arrowFunction.js
@@ -9,3 +9,14 @@ const sum1 = (a, b) => a+b;
 const sum2 = (a, b) => (
     a+b
 );
+
+// print hello world by setInterval function for just 5 times
+
+let id = setInterval(() => {
+    console.log("Hello World");
+}, 2000);
+
+setTimeout(() => {
+    clearInterval(id);
+    console.log("Program ended");
+}, 10000)
---
+++ practice3/practice3.js
@@ -0,0 +1,29 @@
+// ques 1
+//Write an arrow function named arrayAverage that accepts an array of numbers and returns the average of those numbers
+
+let arrayAverage = (arr) => {
+    let sum = 0;
+    let avg = 0;
+    for (let i = 0; i < arr.length; i++){
+        sum += arr[i];
+        //calculate the avg
+        avg = sum / arr.length;
+    }
+    return `The average of the sum of numbers in array is ${avg}`;
+}
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(arrayAverage(arr));
+
+// ques2
+// Write an arrow function named isEven() that takes a single number as argument and returns if it is even or not
+
+let isEven = (num) => {
+    if (num % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+console.log(isEven(10));
