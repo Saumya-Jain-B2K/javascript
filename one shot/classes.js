1c1d27401528447b49351c83ea24893198b95b0d
Saumya-Jain-B2K <saumyajain2020@gmail.com>
2026-08-03 15:59:13 +0530

    done with classes and inbuilt functions

--- one shot/classes.js
+++ one shot/classes.js
@@ -1,35 +1,76 @@
-class Human {
-    //properties
-    age = 10; //public
-    #weight = 20; //to mark any property in class as private we use "#"
-    height = 165;
+// class Human {
+//     //properties
+//     age = 10; //public
+//     #weight = 20; //to mark any property in class as private we use "#"
+//     height = 165;
 
-    //behaviour and functions
-    running() {
-        console.log("I am running", this.#weight); //here this refers to current object value weight that is priate but still can be accessed
-    }
+//     //constructor
+//     constructor (newAge, newHeight, newWeight){
+//         this.age = newAge;
+//         this.height = newHeight;
+//         //setting a private field to be edited
+//         this.#weight = newWeight;
+//     }
 
-    walking() {
-        console.log("I am walking");
-    }
+//     //behaviour and functions
+//     running() {
+//         console.log("I am running", this.#weight); //here this refers to current object value weight that is priate but still can be accessed
+//     }
 
-    //to use private properties ouside the class eaisly we use getter and setter functions
-    get getWeight() {
-        return this.#weight;
-    }
+//     walking() {
+//         console.log("I am walking");
+//     }
 
-    set setWeight(wt) {
-        this.#weight = wt;
-    }
-}
+//     //to use private properties ouside the class eaisly we use getter and setter functions
+//     get getWeight() {
+//         return this.#weight;
+//     }
 
-//creating an object
-let obj = new Human();
-console.log(obj.age);
-//console.log(obj.#weight);  //private
-console.log(obj.height);
-console.log(obj.walking());
-console.log(obj.running());
-console.log(obj.getWeight);
-obj.setWeight = 45;
-console.log(obj.getWeight);
+//     set setWeight(wt) {
+//         this.#weight = wt;
+//     }
+// }
+
+// //creating an object
+// //the consgtructor values are passed when the object is defined itself
+// let obj = new Human(34, 152, 80);
+// console.log(obj.age);
+// //console.log(obj.#weight);  //private
+// console.log(obj.height);
+// console.log(obj.getWeight);
+// console.log(obj.walking());
+// console.log(obj.running());
+// console.log(obj.getWeight);
+// obj.setWeight = 45;
+// console.log(obj.getWeight);
+
+//default parameters
+// function greet(name = "Jhon Doe"){
+//     console.log("My name is: ", name);
+// }
+
+// greet("Ruchika");
+
+// function greetFull(fName = "Jhon", lName = "Doe"){
+//     console.log("The name is: ", fName + " " + lName);
+// }
+// greetFull("Ruchika", "Rathore");
+
+// //when null and undefined is paddes as the parameter in the function
+// function greet(value = 10) {
+//     console.log("Hello ->", value);
+// }
+
+// // greet(null);
+// greet(undefined);
+
+// //function used as default parameter in other function
+// function one(){
+//     return 200;
+// }
+
+// function two(name = "Jhon", age = one(30)){
+//     console.log(name, age);
+// }
+
+// two();
---
+++ one shot/mathDate.js
@@ -0,0 +1,28 @@
+console.log("Inbuilt objects");
+
+//math functions
+
+// console.log(Math.PI);
+// console.log(Math.max(10, 2, 34, 53, 22, 1,23));
+// console.log(Math.min(10, 2, 34, 53, 22, 1,23));
+// console.log(Math.round(45.642));
+// console.log(Math.floor(23.9));
+// console.log(Math.ceil(23.5));
+// console.log(Math.abs(-10));
+// console.log(Math.random()*10);
+// console.log(Math.sqrt(25));
+// console.log(Math.pow(2,3));
+
+//date
+let curr = new Date();
+console.log(curr);
+
+//another way to initialize
+new Date();
+
+//another way 
+let newdate = new Date("July 24 2004 8:00");
+console.log(newdate);
+
+console.log(newdate.getDay());
+console.log(newdate.getFullYear());
